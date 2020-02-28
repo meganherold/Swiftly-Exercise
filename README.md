8e7957dce4c5ef0e0e5c5496bc4f1fcf329fe352
Megan McPherson <meganmcp95@gmail.com>
2020-02-27 20:13:26 -0800

    Project setup and README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Swiftly-Exercise
+
+This is my solution for the [Swiftly iOS coding exercise](https://github.com/Swiftly-Systems/code-exercise-ios) - an iOS app displaying grocery items currently on "Manager Special".
+
+## Installation
+
+
+## Usage
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ Swiftly-Exercise/Swiftly-Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swiftly-Exercise
+//
+//  Created by Megan Herold on 2/27/20.
+//  Copyright © 2020 Megan Herold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
